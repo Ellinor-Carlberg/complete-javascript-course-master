2cf37e125f3398b64e6476c9640bae20bd92e605
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-02-08 17:33:15 +0100

    assignment 1 done

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,26 @@
+//ep 9
+let js = "amazing";
+/* if (js === "amazing") alert("JS is fun"); */
+console.log(40 + 8 + 23 - 10);
+
+//ep10
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+firstName = "Bob";
+console.log(firstName);
+
+let PI = 3.1415;
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+//better than
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+//Assignment 1
+let country = "Sweden";
+let continent = "Europe";
+let population = 10.23;
+console.log(country, continent, population);

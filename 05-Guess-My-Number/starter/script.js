71abcd3ac630183ecd161c9dc215239b0fbe7346
Ellinor Carlberg <ellinor.carlberg@medieinstitutet.se>
2021-02-15 08:42:15 +0100

    first game done

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,64 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+console.log(secretNumber);
+
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const displayScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+const displayBackgroundColor = function (backgroundColor) {
+  document.querySelector('body').style.backgroundColor = backgroundColor;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  //no number
+  if (!guess) {
+    displayMessage('No number');
+
+    //right number
+  } else if (guess === secretNumber) {
+    displayMessage('Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    displayBackgroundColor('#60b347');
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  }
+
+  //When the guess is wrong
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'To high...' : 'To low...');
+      score--;
+      displayScore(score);
+    } else {
+      displayScore(0);
+      displayMessage('You lost the game!');
+      displayBackgroundColor('red');
+    }
+  }
+});
+
+//play again button
+document.querySelector('.again').addEventListener('click', function () {
+  displayMessage('Start guessing...');
+  displayScore(20);
+  displayBackgroundColor('#222');
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('.number').textContent = '?';
+  console.log(secretNumber);
+});
